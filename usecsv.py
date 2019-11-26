8de23ef426ddec33a69483637c0e7b56ab1fc13c
skytreesea <skytreesea@gmail.com>
2019-11-26 15:55:21 +0900

    Update usecsv.py
    Add a description of how to use it.

--- usecsv.py
+++ usecsv.py
@@ -1,5 +1,10 @@
 import csv, os, re
 
+# opencsv is a method, which can open csv-style files
+# you can simply use it by putting the name of the file. 
+# if you want to open "a.csv", then after "import usecsv"
+# opencsv('a.csv') or a = opencsv('a.csv')  
+
 def opencsv(filename):
     f=open(filename, 'r')
     reader=csv.reader(f)
@@ -8,11 +13,23 @@ def opencsv(filename):
         output.append(i)
     return output
 	
+# writecsv is a method, which can change your csv-style list data into a new csv file 
+# if you want to change a list, like a = [[1,2,3],[2,3,4]] into a csv file
+# after import usecsv, then "writecsv('a.csv', a)
+
 def writecsv(filename, the_list):
     with open(filename,'w',newline='') as f:
         a=csv.writer(f, delimiter=',')
         a.writerows(the_list)
-        
+
+# switch is a method, which can change the element that can be changed into figure style into float
+# as you change a csv file into a csv style, all the elements are of string style
+# if you want to change it into float or integer, you can use the method of switch.
+# By changing them into the data of float, you can use it as figure. 
+# a = [['1','2','3'],['2','3',5']] 
+# b = switch(a)
+# b = [[1.0,2.0,3.0],[2.0,3.0,5.0]]
+
 def switch(listName):
     for i in listName:        
         for j in i:
