16fc2799eeaac0a9057867633c50833d1775363a
skytreesea <skytreesea@gmail.com>
2020-08-07 06:39:55 +0900

    명언수집
    명언 수집  
    (Do it 파이썬 생활프로그래밍) 초판 2020: 241페이지

---
+++ quote.py
@@ -0,0 +1,18 @@
+# 명언 수집  (Do it 파이썬 생활프로그래밍) 초판 2020: 218페이지
+import os , re, usecsv
+import urllib.request as ur
+
+from bs4 import BeautifulSoup as bs
+url = 'http://quotes.toscrape.com/' 
+html = ur.urlopen(url)
+soup=bs(html.read(), 'html.parser')
+
+#명언 하나만 가져오기
+print(soup.find_all('span')[0].text)
+
+# quote 명언 하나만 가져오기
+print(soup.find_all('div',{"class":"quote"})[0])
+
+# 해당 페이지의 명언 모두 출력하기
+for i in soup.find_all('div',{"class":"quote"}):
+	print(i.text)
