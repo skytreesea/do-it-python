5f4cbf61c629ef6d1037f27da9bd1b23d614b648
skytreesea <skytreesea@gmail.com>
2020-08-07 06:24:42 +0900

    영어점수직업만족도인과관계
    영어점수 직업만족도 인과관계  
    (Do it 파이썬 생활프로그래밍) 초판 2020: 218페이지

---
+++ regression.py
@@ -0,0 +1,40 @@
+# 영어점수 직업만족도 인과관계  (Do it 파이썬 생활프로그래밍) 초판 2020: 218페이지
+import os, re, usecsv
+import numpy as np
+
+# 데이터프레임 다루기 
+import pandas as pd
+# 기초 통계분석 패키지
+from scipy import stats
+# 그래프 그리기  
+import matplotlib.pyplot as plt
+#회귀분석 가능 패키지
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+os.chdir(r'J:\one-drive 20200728\MyScript\python\final_git_hub')
+
+# 직업만족도
+# survey 변수들:  name sex  income  English  jobSatisfaction  stress
+df = pd.read_csv("survey.csv")
+
+# 독립표본 t-검정
+# 변수생성
+male = df.income[df.sex == "m"] # 남성
+female = df.income[df.sex == "f"] # 여성
+
+#Levene의 등분산 검정 
+l_result = stats.levene(male, female)
+
+# 유의 수준 표시하기 
+if l_result[1] > .05:
+    print('P value는 %f 로 95 수준에서 유의하지 않음' % l_result[1])
+else :
+    print('P value는 %f 로 95 percent 수준에서 유의함' % l_result[1])
+
+print( '남성', round(male.mean(),2), '여성',round(female.mean(),2),'\n등분산검정 결과 LeveneResult(F) : %.3f \np-value : %.3f' % (l_result)) 
+
+# 가상의 분석자료로 회귀분석 결과 구하기 
+model = smf.ols(formula = 'jobSatisfaction ~ English + stress + income', data = df)
+result = model.fit()
+print(result.summary())
