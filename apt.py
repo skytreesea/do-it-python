b63fe2d9e9741ba7ae95c325fab0e9c903f3d820
skytreesea <skytreesea@gmail.com>
2020-08-07 05:24:01 +0900

    부동산실거래가다루기
    부동산실거래가 다루기 csv  
    (Do it 파이썬 생활프로그래밍) 초판 2020: 155페이지

---
+++ apt.py
@@ -0,0 +1,47 @@
+import usecsv, os, re
+from switch import switch 
+os.chdir(r'C:\Users\지재지\OneDrive - Office Everyday\MyScript\python\manuscript_Book\Data\realestate')
+
+apt = switch(usecsv.opencsv('apt_201910.csv'))
+#apt = usecsv.makenewlist(apt)
+#print(apt[3])
+header = ['시군구', '번지', '본번', '부번', '단지명', '전용면적(㎡)', '계약년월', '계약일', '거래금액(만원)', '층', '건축년도', '도로명'] 
+newlist = []
+for i in apt:
+	try: 
+	#조건1: 100㎡ 이상, 10층에서 15층 사이, 5억이상
+		#if i[5]> 100 and i [-3] > 10 and i [-3] < 15 and i[-4] > 50000:
+	#조건2: 130㎡ 이상,4억 이하
+		#if i[5] > 130 and i[-4] < 30000:
+			#출력조건: 지역, 단지명, 크기, 층수, 거래금액 
+			#newlist.append([re.match('[가-힣]+ [가-힣]+',i[0]).group(), i[4], i[5], i[-3], i[-4]])
+		#조건3  단지명 아이파크
+		#if re.search('래미안',i[4]):
+			#출력조건: 지역, 단지명, 크기, 층수, 거래금액 
+			#newlist.append([re.match('[가-힣]+ [가-힣]+',i[0]).group(), i[4], i[5], i[-3], i[-4]])
+	#조건4: 20억 이상
+		if i[-4]  > 150000:
+			#출력조건: 지역, 단지명, 크기, 층수, 거래금액 
+			newlist.append([re.match('[가-힣]+ [가-힣]+',i[0]).group(), i[4], i[5], i[-3], i[-4]])
+	except: pass
+
+print(newlist[0])
+region = list(set([i[0] for i in newlist]))
+summary = [ ]
+nameApt = list(set([i[1] for i in newlist]))
+price = [i[-1] for i in newlist]
+
+k=0
+# 각 지역에 해당 조건 아파트가 몇 채 있는지 summary
+for j in region:
+	for i in newlist:
+		if i[0] == j:
+			k += 1
+	summary.append([j,k])
+	k=0
+usecsv.writecsv('apt_over150,000.csv', newlist)
+usecsv.writecsv('apt_sum_over150,000.csv', summary)
+
+import numpy as np 
+price2=np.array([price])
+print(price2.mean(), price2.max())
