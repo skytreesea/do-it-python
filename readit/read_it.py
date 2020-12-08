d61bb56c65308dd75e39dfac431cdaf3c8c7ae17
skytreesea <skytreesea@gmail.com>
2020-12-08 10:57:16 +0900

    read_it.py
    보고서 읽어주는 파일

---
+++ readit/read_it.py
@@ -0,0 +1,24 @@
+import csv, os, re
+# 
+os.chdir(r'C:\Users\user\Documents\지방공기업평가원_김창현\타당성검토\설립 타당성\타당성검토\완도해양치유공단\보고서\자동화')
+def opencsv(filename):
+    f=open(filename, 'r')
+    reader=csv.reader(f)
+    output=[]
+    for i in reader:
+        output.append(i)
+    return output
+
+total = opencsv('pop_region.csv')
+def crease(figure):
+    if re.search('-',figure):
+        return "감소"
+    else: 
+        return "증가"
+start_year = total[0][1]
+end_year = total[0][3]
+
+with open('read_it.txt','w') as f:
+    for i in total[2:]:
+        creasing = crease(i[-2])
+        f.write('\n%s의 %s년 인구는 %s명(%s)으로 %s년 %s명(%s)에 비하여 %s명 %s함, 점유율 변동(%s)' % (i[0], end_year, i[3],i[4],start_year,i[1],i[2],re.sub('-','',i[-2]), creasing, i[-1]))
