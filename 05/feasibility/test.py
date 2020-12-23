2fda0abbcde1a1d133e1c6b5b2579ffbc3526051
skytreesea <skytreesea@gmail.com>
2020-12-24 05:07:52 +0900

    complete_the_elements_of_results
    결과값은 완성, 데이터 입력을 쉽게하는 함수 개발 필요

---
+++ 05/feasibility/test.py
@@ -0,0 +1,12 @@
+#퍼센트 만들기 
+percent = lambda x: str(round(x*100,2))+'%'
+print(percent(0.011))
+a = ['가','0.2233','23']
+def percent_list(list_of_percent) : 
+    new = [percent(float(i)) for  i in list_of_percent[1:]]
+    new.insert(0,list_of_percent[0])
+    return new
+print(percent_list(a))
+
+test = [[1,2,3],['구분',	'2022',	'2023']]
+print(percent_list(test[1]))
