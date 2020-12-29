4da9fd117e6d9c05feeaf4b7c8e714b4984ca596
skytreesea <skytreesea@gmail.com>
2020-12-30 07:49:17 +0900

    model_update
    change_the_variable, add a comma etc.

--- 05/feasibility/feasible_ver1.8_suc.py
+++ 05/feasibility/feasible_ver1.8_suc.py
@@ -38,13 +38,17 @@ def line(np_row):
     return new
 #퍼센트 만들기 
 percent = lambda x: str(round(x*100,2))+'%'
-data_basic = opencsv('주요데이터입력(취합).csv')
+data_basic = opencsv('seogu_k.csv')
 
-# 2번째 요소부터 퍼센트가 들어있는 리스트를 퍼센트 형태로 바꿔주는 함수 
 def percent_list(list_of_percent) : 
+    # 2번째 요소부터 퍼센트가 들어있는 리스트를 퍼센트 형태로 바꿔주는 함수 
     new = [percent(float(i)) for  i in list_of_percent[1:]]
     new.insert(0,list_of_percent[0])
     return new
+
+def add_comma(list_starting_with_str):
+    #첫번째 요소가 문자이고 다음부터 숫자로 된 숫자 리스트일 경우 콤마 찍어주는 프로그램
+    return [list_starting_with_str[0]]+[format(round(float(j)),',') for j in list_starting_with_str if re.search('\d{3}',j) ]
 
 def main(number):
 #숫자(number)를 받아서 csv형 리스트를 반환하도록 함
@@ -52,8 +56,8 @@ def main(number):
     data_name = [i for i in data_basic[number][2:]]
     data = [float(i) for i in data_basic[number][2:]]
 # 현행방식과 공기업 방식의 인플레이션 인덱스 부여
-# # 물가상승률
-    price_index = data[6]
+# # 비용상승률
+    cost_increase = data[6]
 # # 인건비 상승률
     salary_increase =data[7]
 # # 대상사업수입증가율
@@ -66,12 +70,12 @@ def main(number):
 # # 분석기간(년도)
     num_years = 5
     result_list=[]
-# # 현행 수입: 물가상승률
-    result_list.append(inflator(data[0],price_index, num_years))
+# # 현행 수입상승률
+    result_list.append(inflator(data[0],income_increase, num_years))
 # # 현행 인건비: 인건비 상승률 
     result_list.append(inflator(data[1],salary_increase, num_years))
 # # 현행 운영비: 물가상승률
-    result_list.append(inflator(data[2],price_index, num_years))
+    result_list.append(inflator(data[2],cost_increase, num_years))
 # # 공단 수입: 수입상승률
     result_list.append(inflator(data[3],income_increase, num_years))
 # # 공단 인건비: 인건비 상승률
@@ -80,8 +84,11 @@ def main(number):
     result_list.append(inflator(data[5],operation_increase_new_company, num_years))
     array = np.array(result_list)
 # # 현행방식, 공단방식 경상수지 비율
-    balance_current = np.array(array[0]/(array[1]+array[2]))
-    balance_new = array[3]/(array[4]+array[5])
+    if (array[1]+array[2]).any() and (array[4]+array[5]).any():
+        balance_current = np.array(array[0]/(array[1]+array[2]))
+        balance_new = array[3]/(array[4]+array[5])
+    else:
+        print(name+'에러')
 # # 현행방식 공단방식 이익
     profit_current = array[0]-(array[1]+array[2])
     profit_new = array[3]-(array[4]+array[5])
@@ -123,17 +130,17 @@ def main(number):
 # # np로 계산한 것을 리스트로 만들어 csv로 저장, 출력하기 쉬운 형태로 변형
     total=[]
     total.append(['사업명',	name])
-    total.append(['',	'물가상승률','인건비상승률',	'수입증가율'+percent(income_increase),	'인건비상승률',	'시작년도'	])
-    total.append(['',percent(price_index),percent(salary_increase),percent(income_increase),percent(salary_increase_new_company),str(start_year),'(단위:천원)'])
+    total.append(['',	'운영비상승률(대체로 물가상승률 적용)','인건비상승률',	'수입증가율(대체로 현행/공단 같이 적용', '공단 인건비상승률',	'','시작년도'	])
+    total.append(['',percent(cost_increase),percent(salary_increase),percent(income_increase),percent(salary_increase_new_company),'',str(start_year),'(단위:천원)'])
     total.append(['현행'])
-    total.append(['구분','2022',	'2023',	'2024',	'2025',	'2026',	'계',	'평균'])
+    total.append(['구분','2022','2023',	'2024',	'2025',	'2026',	'계',	'평균'])
     k=0
     for i in label_plus:  
         if re.search('비율',i[0]):
             total 
             total.append(percent_list(i))
         else: 
-            total.append(i)
+            total.append(add_comma(i))
         if k == 5:
             total.append(['공단방식'])
             total.append(['구분',	'2022',	'2023',	'2024',	'2025',	'2026',	'계',	'평균'])
@@ -147,8 +154,9 @@ def main(number):
         final_result =  "불가능"
         total.append(['미충족', '경상수지 비율:', percent(balance_new.mean()),final_result] )
     writecsv(name+'_'+'.csv',total)
+ 
+os.chdir(r'C:\Users\ERC\Documents\GitHub\do-it-python\05\feasibility\data\seogu')
 
-os.chdir(r'C:\Users\ERC\Documents\GitHub\do-it-python\05\feasibility\data\results')
 # # 최종명령
 for i in range(1,len(data_basic)):
     try:
