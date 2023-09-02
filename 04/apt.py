4f0df4c267e0493eb4cc9ab81174829c9b0e2743
Kim Changhyun <skytreesea@gmail.com>
2023-09-02 11:14:51 +0900

    update

--- 04/apt.py
+++ 04/apt.py
@@ -1,48 +1,37 @@
-import usecsv, os, re
-from switch import switch 
-# 자기 경로를 입력해야 합니다. 
-os.chdir(r' ')
+import usecsv, csv, re
+# 자기 경로를 입력해야 합니다.  
 # csv 파일이 같은 폴더에 있는지 먼저 확인하세요.
-apt = switch(usecsv.opencsv('apt_201910.csv'))
-#apt = usecsv.makenewlist(apt)
-#print(apt[3])
-header = ['시군구', '번지', '본번', '부번', '단지명', '전용면적(㎡)', '계약년월', '계약일', '거래금액(만원)', '층', '건축년도', '도로명'] 
-newlist = []
+#apt = usecsv.switch(usecsv.opencsv(r'C:\Users\skytr\Documents\GitHub\do-it-python\05\\apt2308.csv'))
+apt=usecsv.opencsv(r'C:\Users\skytr\Documents\GitHub\do-it-python\05\\apt2308.csv' )
+apt = usecsv.switch(apt)
+
+# 불러오는 데 성공 
+for i in apt[:6]:
+    print(i[0],i[4],i[-8]) 
+    
+for i in apt:
+	try:
+# 오류가 날 경우를 대비해 예외 처리를 사용하겠습니다
+		if i[5] >= 120 and i[8] <= 30000 and re.match('강원', i[0]):
+# 면적(i[5]) 120㎡ 이상, 가격(i[8]) 3억 이하, 시군구(i[0])에는 '강원'을 포함하는 조건입니다
+			print(i[0], i[4], i[8])
+# 시군구, 아파트 단지명, 가격 순으로 출력합니다. 
+	except: 
+		pass
+
+new_list = []
+# 어떤 이름으로든 빈 리스트를 만드는 것이 CSV형 리스트를 만드는 첫걸음입니다
+
 for i in apt:
 	try: 
-	#조건1: 100㎡ 이상, 10층에서 15층 사이, 5억이상
-		#if i[5]> 100 and i [-3] > 10 and i [-3] < 15 and i[-4] > 50000:
-	#조건2: 130㎡ 이상,4억 이하
-		#if i[5] > 130 and i[-4] < 30000:
-			#출력조건: 지역, 단지명, 크기, 층수, 거래금액 
-			#newlist.append([re.match('[가-힣]+ [가-힣]+',i[0]).group(), i[4], i[5], i[-3], i[-4]])
-		#조건3  단지명 아이파크
-		#if re.search('래미안',i[4]):
-			#출력조건: 지역, 단지명, 크기, 층수, 거래금액 
-			#newlist.append([re.match('[가-힣]+ [가-힣]+',i[0]).group(), i[4], i[5], i[-3], i[-4]])
-	#조건4: 20억 이상
-		if i[-4]  > 150000:
-			#출력조건: 지역, 단지명, 크기, 층수, 거래금액 
-			newlist.append([re.match('[가-힣]+ [가-힣]+',i[0]).group(), i[4], i[5], i[-3], i[-4]])
-	except: pass
+		if i[5] >= 120 and i[8] <= 30000 and re.match('강원', i[0]):
+# 앞에서 만든 조건을 그대로 입력합니다			
+			new_list.append([i[0], i[4], i[8]])
+# 앞에서 출력했던 시군구, 아파트 단지명, 가격을 new_list에 저장합니다 
+	except: 
+		pass
 
-print(newlist[0])
-region = list(set([i[0] for i in newlist]))
-summary = [ ]
-nameApt = list(set([i[1] for i in newlist]))
-price = [i[-1] for i in newlist]
+print(new_list)
+usecsv.writecsv(r'C:\Users\skytr\Documents\GitHub\do-it-python\05\over120_lower30000.csv', new_list)
+# writecsv() 함수로 new_list 객체에 저장된 CSV형 리스트를 CSV 파일로 저장합니다
 
-k=0
-# 각 지역에 해당 조건 아파트가 몇 채 있는지 summary
-for j in region:
-	for i in newlist:
-		if i[0] == j:
-			k += 1
-	summary.append([j,k])
-	k=0
-usecsv.writecsv('apt_over150,000.csv', newlist)
-usecsv.writecsv('apt_sum_over150,000.csv', summary)
-
-import numpy as np 
-price2=np.array([price])
-print(price2.mean(), price2.max())
