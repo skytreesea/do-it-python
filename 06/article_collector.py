529e84de1e7c3348691437e75f5a16e57a120720
Kim Changhyun <skytreesea@gmail.com>
2023-09-24 08:37:56 +0900

    change the class of div in html page

--- 06/article_collector.py
+++ 06/article_collector.py
@@ -1,10 +1,11 @@
 import os , requests, datetime
 from bs4 import BeautifulSoup  
-os.chdir(r'C:\Users\skytr\Documents\GitHub\do-it-python\06')
+# 저장하고 싶은 경로를 추가합니다. (의무 아님)
+# os.chdir(r'C:\Users\skytr\Documents\GitHub\do-it-python\06')
 url = 'https://news.daum.net/'
 f = open(str(datetime.date.today()) + '_articles.txt','w')
 soup = BeautifulSoup(requests.get(url).text)
-for i in soup.find_all('div',{"class":"item_issue"}):
+for i in soup.find_all('div',{"class":"cont_thumb"}):
     try:
         f.write(i.text+'\n')
         f.write(i.find('a').get('href') + '\n')
