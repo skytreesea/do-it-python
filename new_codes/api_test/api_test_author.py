e3d0c35de25626f06a21ea4c69eaa6491d6742dc
Kim Changhyun <skytreesea@gmail.com>
2023-10-01 07:30:21 +0900

    update api_test_author.py

--- new_codes/api_test/api_test_author.py
+++ new_codes/api_test/api_test_author.py
@@ -2,7 +2,7 @@
 import requests 
 from bs4 import BeautifulSoup
 
-api_key = 'your_key'
+api_key = '7JlKxM7fEbOErQRa32MtR3%2Fg%2FBxi3JTPbwPfCw781Ma4uvwql5x2r2wM0Zh051RRUK%2Bw7YSwijxr0Tklej3cOg%3D%3D'
 # 내 서비스키를 이용해서 만든 예제 코드 
 base_url = f'http://apis.data.go.kr/9720000/searchservice/basic?serviceKey={api_key}&pageno=1&displaylines=10&search=자료명,미국'
  
@@ -22,10 +22,23 @@ for item in soup.find_all('item'):
 for item in soup.find_all('item'):
     if item.find('name').text == '기사명':
         print(item.find('value').text)
-     
-# 저자명과 기사명 
+        
+total = []     
+each_item = []
+
 for item in soup.find_all('item'):
     if item.find('name').text == '기사명':
-        print(item.find('value').text)
-    if item.find('name').text == '저자명':
-        print(item.find('value').text)
+        each_item.append(item.find('value').text)
+    elif item.find('name').text == '저자명':
+        each_item.append(item.find('value').text)
+    # 둘 다 저장되면 total에 추가하고 each_item 초기화
+    if len(each_item) == 2:
+        total.append(each_item)
+        each_item = []
+    
+print(total)
+
+import pandas as pd 
+df = pd.DataFrame(total, columns=['기사명','저자명'])
+print(df)
+ 
